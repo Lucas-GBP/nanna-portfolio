d870376047098ee751cbf5285d20ba3339b03b0e
Lucas-GBP <lucas.bitencourte.pedroso@gmail.com>
2026-08-07 23:13:50 -0300

    multple languages

---
+++ src/i18n/request.ts
@@ -0,0 +1,20 @@
+import {cookies} from "next/headers";
+import {getRequestConfig} from "next-intl/server";
+
+const locales = ["en", "pt"] as const;
+type Locale = (typeof locales)[number];
+
+function isLocale(value: string | undefined): value is Locale {
+  return locales.some((locale) => locale === value);
+}
+
+export default getRequestConfig(async () => {
+  const store = await cookies();
+  const requestedLocale = store.get("locale")?.value;
+  const locale = isLocale(requestedLocale) ? requestedLocale : "en";
+
+  return {
+    locale,
+    messages: (await import(`../messages/${locale}.json`)).default,
+  };
+});
